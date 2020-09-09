8ea2ab00c3e28be79e6d566a322bce87bcf0f7dc
razrow <razrow@gmail.com>
2020-09-08 22:23:38 -0700

    Fixed SplashScreen background to look like blackboard.

--- app/src/main/java/com/example/cst438_project1/SplashScreen.java
+++ app/src/main/java/com/example/cst438_project1/SplashScreen.java
@@ -3,15 +3,28 @@ package com.example.cst438_project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    TextView mTopText;
+    TextView mBottomText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        mTopText = findViewById(R.id.AppName);
+        mBottomText = findViewById(R.id.AppName2);
+
+        mTopText.setTextColor(Color.rgb(246,244,241));
+        mBottomText.setTextColor(Color.rgb(246,244,241));
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
