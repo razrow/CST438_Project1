7c309a227f4a083d5f2edca62f795bf498f5cdc5
Charlie Nguyen <36280021+charliekid@users.noreply.github.com>
2020-09-10 20:32:36 -0700

    updated unit test to test against db

--- app/src/test/java/com/example/cst438_project1/UserTest.java
+++ app/src/test/java/com/example/cst438_project1/UserTest.java
@@ -1,13 +1,35 @@
 package com.example.cst438_project1;
 
+
+import android.content.Context;
+
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+
+
+import com.example.cst438_project1.UserDB;
+import com.example.cst438_project1.UserDAO;
+
+
 public class UserTest {
     private static final String[] USER_INFO = {"Jordan", "Doe", "JordanDoe1", "password1"};
     private static final String CORRECT_CONSTRUCTOR_TOSTRING = "username = JordanDoe1\n" +
                                                                 "first name = Jordan\n" +
                                                                 "last name = Doe\n" +
                                                                 "password = password1";
+
+    private UserDAO userDao;
+    private UserDB db;
+
+    @Before
+    public void createDb() {
+        //Context context = androidx.test.core.app.ApplicationProvider.getApplicationContext();
+
+    }
+
+
     @Test
     public void testConstructor() {
         User user = new User(USER_INFO[0], USER_INFO[1], USER_INFO[2], USER_INFO[3]);
@@ -31,8 +53,8 @@ public class UserTest {
     @Test
     public void testGetters() {
         User user1 = new User(USER_INFO[0], USER_INFO[1], USER_INFO[2], USER_INFO[3]);
-        assertEquals("Expected and actual DON'T match", USER_INFO[0], user1.getfName());
-        assertEquals("Expected and actual DON'T match", USER_INFO[1], user1.getlName());
+        assertEquals("Expected and actual DON'T match", USER_INFO[0], user1.getFName());
+        assertEquals("Expected and actual DON'T match", USER_INFO[1], user1.getLName());
         assertEquals("Expected and actual DON'T match", USER_INFO[2], user1.getUsername());
     }
 
