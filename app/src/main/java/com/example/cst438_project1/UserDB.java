7d6208a82616ba399ffc6059a8d4bd73e091ceae
Charlie Nguyen <36280021+charliekid@users.noreply.github.com>
2020-09-12 23:32:10 -0700

    Added and adjusted for Assignment test I had to update the version code for this heads up.

--- app/src/main/java/com/example/cst438_project1/UserDB.java
+++ app/src/main/java/com/example/cst438_project1/UserDB.java
@@ -9,7 +9,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {User.class, Grade.class, Enrollment.class, Course.class, Category.class, Assignment.class}, version = 1 )
+@Database(entities = {User.class, Grade.class, Enrollment.class, Course.class, Category.class, Assignment.class}, version = 2 )
 public abstract class UserDB extends RoomDatabase {
 
     public static final String DB_NAME = "user_db";
@@ -17,11 +17,13 @@ public abstract class UserDB extends RoomDatabase {
 
     public abstract UserDAO userDao();
     public abstract CourseDAO courseDao();
+    public abstract AssignmentDAO assignmentDao();
 
     public static synchronized UserDB getUserDAO(Context context){
         if(instance == null){
             instance = Room.databaseBuilder(context.getApplicationContext(), UserDB.class, DB_NAME)
                     .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()       // needs for Junit testing (version increment)
                     .build();
         }
         return instance;
@@ -40,10 +42,12 @@ public abstract class UserDB extends RoomDatabase {
     private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
         private UserDAO userDao;
         private CourseDAO courseDao;
+        private AssignmentDAO assignmentDao;
 
         private PopulateDbAsyncTask(UserDB db) {
             userDao = db.userDao();
             courseDao = db.courseDao();
+            assignmentDao = db.assignmentDao();
         }
 
         @Override
@@ -51,6 +55,7 @@ public abstract class UserDB extends RoomDatabase {
             userDao.insert(new User("Tina1", "boyz123", "Tina", "Belcher"));
             courseDao.insert(new Course("Jairo", "Capoeira Class",
                                         "Sexy Dance Fighting", "01/01/01", "03/01/01"));
+            assignmentDao.insert(new Assignment("Read 500 books", 100, 100));
             return null;
         }
     }
